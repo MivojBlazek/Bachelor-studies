e56b320b606e56792320e887fa233338aadece11
MivojBlazek <MivojBlazek@seznam.cz>
2022-12-05 22:32:12 +0100

    AdventOfCode day5

--- C/AdventOfCode/2022/day5/day5.c
+++ C/AdventOfCode/2022/day5/day5.c
@@ -1,55 +1,87 @@
 #include <stdio.h>
 #include <string.h>
 
-//char line[50];
-char tower[9][60];
+char tower[9][100];
+char tmp[100];
 char final_str[9];
 
-int main()
+int main(int argc)
 {
-	int input_move, input_from, input_to;
-	char c1, c2, c3, c4, c5, c6, c7, c8, c9;
-	FILE *input;
-	input = fopen("day5.txt", "r");
-	for (int i = 0; fscanf(input, "[%c] [%c] [%c] [%c] [%c] [%c] [%c] [%c] [%c]", c1, c2, c3, c4, c5, c6, c7, c8, c9) != 0; i++)
-	{
-		tower[0][i] = c1;
-		tower[1][i] = c2;
-		tower[2][i] = c3;
-		tower[3][i] = c4;
-		tower[4][i] = c5;
-		tower[5][i] = c6;
-		tower[6][i] = c7;
-		tower[7][i] = c8;
-		tower[8][i] = c9;
-	}
-	
-	//debug test
-	//
-	for (int i = 0; i < 9; i++)
-	{
-		fprintf(stderr, "%d. ", i);
-		for (int j = 0; j < 60; j++)
-		{
-			fprintf(stderr, "%c ", tower[i][j]);
-		}
-		fprintf(stderr, "\n");
-	}
-	//
-	
-	while (fscanf(input, "move %d from %d to %d", input_move, input_from, input_to) != EOF)
-	{
-		for (int i = 0; i < input_move; i++)
-		{
-			strncat(tower[input_to - 1], &tower[input_from - 1][0], 1);
-		}
-	}
+    int input_move, input_from, input_to;
+    char c;
+    FILE *input;
+    input = fopen("day5.txt", "r");
 
-	for (int i = 0; i < 9; i++)
-	{
-		strncat(final_str, &tower[i][strlen(tower[i]) - 1], 1);
-	}
-	printf("Final string is: %s\n", final_str);
-	fclose(input);
-	return 0;
+    //nacte uvodni pozici pismen
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; (c = fgetc(input)) != '\n'; j++)
+        {
+            if (c >= 'A' && c <= 'Z')
+            {
+                strncat(tower[j / 4], &c, 1);
+            }
+        }
+    }
+
+    //otoci radky (reverse)
+    for (int i = 0; i < 9; i++)
+    {
+        for (int j = 0; j < strlen(tower[i]) / 2; j++)
+        {
+            char tmp_c = tower[i][j];
+            tower[i][j] = tower[i][strlen(tower[i]) - 1 - j];
+            tower[i][strlen(tower[i]) - 1 - j] = tmp_c;
+        }
+        tower[i][strlen(tower[i])] = '\0';
+    }
+
+    //cte zbytek souboru a provadi pohyby
+    while (fscanf(input, "move %d from %d to %d\n", &input_move, &input_from, &input_to) != EOF)
+    {
+        input_from--;
+        input_to--;
+        if (argc == 2)
+        {
+            for (int i = 0; i < input_move; i++)
+            {
+                strncat(tower[input_to], &tower[input_from][strlen(tower[input_from]) - 1], 1);
+                tower[input_from][strlen(tower[input_from]) - 1] = '\0';
+                tower[input_to][strlen(tower[input_to])] = '\0';
+            }
+        }
+        else
+        {
+            for (int i = 0; i < input_move; i++)
+            {
+                strncat(tmp, &tower[input_from][strlen(tower[input_from]) - 1], 1);
+                tower[input_from][strlen(tower[input_from]) - 1] = '\0';
+            }
+
+            //otoci radky (reverse)
+            for (int j = 0; j < strlen(tmp) / 2; j++)
+            {
+                char tmp_char = tmp[j];
+                tmp[j] = tmp[strlen(tmp) - 1 - j];
+                tmp[strlen(tmp) - 1 - j] = tmp_char;
+            }
+
+            strcat(tower[input_to], tmp);
+
+            tower[input_to][strlen(tower[input_to])] = '\0';
+        }
+        for (int i = 0; i < 100; i++)
+        {
+            tmp[i] = '\0';
+        }
+    }
+    
+    //vypise posledni pismena jako string
+    for (int i = 0; i < 9; i++)
+    {
+        strncat(final_str, &tower[i][strlen(tower[i]) - 1], 1);
+    }
+    printf("Final string is: %s\n", final_str);
+    fclose(input);
+    return 0;
 }
