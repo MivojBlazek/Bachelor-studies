b0cf7629a87ba22d99ff09ee3ab55350d00c4dc0
MivojBlazek <MivojBlazek@seznam.cz>
2023-04-23 16:25:37 +0200

    IZG changes, nothing is working

--- IZG/izgProject/build/CMakeFiles/izgProject.dir/student/gpu.cpp.o.d
+++ IZG/izgProject/build/CMakeFiles/izgProject.dir/student/gpu.cpp.o.d
@@ -1,13 +1,7 @@
 CMakeFiles/izgProject.dir/student/gpu.cpp.o: \
  /home/michal/Coding/IZG/izgProject/student/gpu.cpp \
- /usr/include/stdc-predef.h \
- /home/michal/Coding/IZG/izgProject/student/gpu.hpp \
- /home/michal/Coding/IZG/izgProject/student/fwd.hpp \
- /home/michal/Coding/IZG/izgProject/libs/glm-0.9.9.8/glm/glm.hpp \
- /home/michal/Coding/IZG/izgProject/libs/glm-0.9.9.8/glm/detail/_fixes.hpp \
- /usr/include/c++/11/cmath \
- /usr/include/x86_64-linux-gnu/c++/11/bits/c++config.h \
- /usr/include/x86_64-linux-gnu/c++/11/bits/os_defines.h \
+ /usr/include/stdc-predef.h /usr/include/stdio.h \
+ /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
  /usr/include/features.h /usr/include/features-time64.h \
  /usr/include/x86_64-linux-gnu/bits/wordsize.h \
  /usr/include/x86_64-linux-gnu/bits/timesize.h \
@@ -15,18 +9,34 @@ CMakeFiles/izgProject.dir/student/gpu.cpp.o: \
  /usr/include/x86_64-linux-gnu/bits/long-double.h \
  /usr/include/x86_64-linux-gnu/gnu/stubs.h \
  /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/lib/gcc/x86_64-linux-gnu/11/include/stddef.h \
+ /usr/lib/gcc/x86_64-linux-gnu/11/include/stdarg.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+ /usr/include/x86_64-linux-gnu/bits/time64.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__fpos_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__mbstate_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__fpos64_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/types/FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/types/cookie_io_functions_t.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/floatn.h \
+ /usr/include/x86_64-linux-gnu/bits/floatn-common.h \
+ /home/michal/Coding/IZG/izgProject/student/gpu.hpp \
+ /home/michal/Coding/IZG/izgProject/student/fwd.hpp \
+ /home/michal/Coding/IZG/izgProject/libs/glm-0.9.9.8/glm/glm.hpp \
+ /home/michal/Coding/IZG/izgProject/libs/glm-0.9.9.8/glm/detail/_fixes.hpp \
+ /usr/include/c++/11/cmath \
+ /usr/include/x86_64-linux-gnu/c++/11/bits/c++config.h \
+ /usr/include/x86_64-linux-gnu/c++/11/bits/os_defines.h \
  /usr/include/x86_64-linux-gnu/c++/11/bits/cpu_defines.h \
  /usr/include/c++/11/pstl/pstl_config.h \
  /usr/include/c++/11/bits/cpp_type_traits.h \
  /usr/include/c++/11/ext/type_traits.h /usr/include/math.h \
- /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
- /usr/include/x86_64-linux-gnu/bits/types.h \
- /usr/include/x86_64-linux-gnu/bits/typesizes.h \
- /usr/include/x86_64-linux-gnu/bits/time64.h \
  /usr/include/x86_64-linux-gnu/bits/math-vector.h \
  /usr/include/x86_64-linux-gnu/bits/libm-simd-decl-stubs.h \
- /usr/include/x86_64-linux-gnu/bits/floatn.h \
- /usr/include/x86_64-linux-gnu/bits/floatn-common.h \
  /usr/include/x86_64-linux-gnu/bits/flt-eval-method.h \
  /usr/include/x86_64-linux-gnu/bits/fp-logb.h \
  /usr/include/x86_64-linux-gnu/bits/fp-fast.h \
@@ -35,7 +45,6 @@ CMakeFiles/izgProject.dir/student/gpu.cpp.o: \
  /usr/include/x86_64-linux-gnu/bits/mathcalls-narrow.h \
  /usr/include/x86_64-linux-gnu/bits/iscanonical.h \
  /usr/include/c++/11/bits/std_abs.h /usr/include/stdlib.h \
- /usr/lib/gcc/x86_64-linux-gnu/11/include/stddef.h \
  /usr/include/x86_64-linux-gnu/bits/waitflags.h \
  /usr/include/x86_64-linux-gnu/bits/waitstatus.h \
  /usr/include/x86_64-linux-gnu/bits/types/locale_t.h \
--- IZG/izgProject/student/gpu.cpp
+++ IZG/izgProject/student/gpu.cpp
@@ -4,10 +4,31 @@
  *
  * @author Tomáš Milet, imilet@fit.vutbr.cz
  */
-
+#include <stdio.h> //! POUZE PRO TEST
 #include <student/gpu.hpp>
 
-
+void clear(GPUMemory &mem, ClearCommand cmd){
+  if(cmd.clearColor)
+  {
+    float red = cmd.color.r;
+    float green = cmd.color.g;
+    float blue = cmd.color.b;
+    float alpha = cmd.color.a;
+    
+    mem.framebuffer.color[0] = (uint8_t) (red * 255.f);
+    mem.framebuffer.color[1] = (uint8_t) (green * 255.f);
+    mem.framebuffer.color[2] = (uint8_t) (blue * 255.f);
+    mem.framebuffer.color[3] = (uint8_t) (alpha * 255.f);
+    
+    fprintf(stderr, "red: %f\nmem: %d\n", red, mem.framebuffer.color[0]); //! DEBUG
+  }
+  if(cmd.clearDepth)
+  {
+    float depth = cmd.depth;
+    mem.framebuffer.depth[0] = depth; //? not sure [0]
+    //TODO vycisti Depth
+  }
+}
 
 //! [gpu_execute]
 void gpu_execute(GPUMemory&mem,CommandBuffer &cb){
@@ -18,6 +39,17 @@ void gpu_execute(GPUMemory&mem,CommandBuffer &cb){
   /// mem obsahuje paměť grafické karty.
   /// cb obsahuje command buffer pro zpracování.
   /// Bližší informace jsou uvedeny na hlavní stránce dokumentace.
+  for (uint32_t i = 0; i < cb.nofCommands; i++)
+  {
+    if (cb.commands[i].type == CommandType::CLEAR)
+    {
+      clear(mem, cb.commands[i].data.clearCommand);
+    }
+    else if (cb.commands[i].type == CommandType::DRAW)
+    {
+      //TODO kresleni
+    }
+  }
 }
 //! [gpu_execute]
 
