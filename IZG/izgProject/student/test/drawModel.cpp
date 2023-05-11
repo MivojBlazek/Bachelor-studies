793a0eec515108761d78a670e5b59294be1ed0b5
MivojBlazek <MivojBlazek@seznam.cz>
2023-05-11 21:11:31 +0200

    IZG project zipped

---
+++ IZG/izgProject/student/test/drawModel.cpp
@@ -0,0 +1,217 @@
+/*!
+ * @file
+ * @brief This file contains functions for model rendering
+ *
+ * @author Tomáš Milet, imilet@fit.vutbr.cz
+ */
+#include <student/drawModel.hpp>
+#include <student/gpu.hpp>
+
+///\endcond
+
+void addDrawCommand(CommandBuffer &cb, Mesh const &mesh)
+{
+  cb.commands[cb.nofCommands].type = CommandType::DRAW;
+  if (mesh.doubleSided)
+  {
+    cb.commands[cb.nofCommands].data.drawCommand.backfaceCulling = false;
+  }
+  else
+  {
+    cb.commands[cb.nofCommands].data.drawCommand.backfaceCulling = true;
+  }
+  cb.commands[cb.nofCommands].data.drawCommand.nofVertices = mesh.nofIndices;
+  cb.commands[cb.nofCommands].data.drawCommand.programID = 0;
+
+  cb.commands[cb.nofCommands].data.drawCommand.vao.indexBufferID = mesh.indexBufferID;
+  cb.commands[cb.nofCommands].data.drawCommand.vao.indexOffset = mesh.indexOffset;
+  cb.commands[cb.nofCommands].data.drawCommand.vao.indexType = mesh.indexType;
+
+  cb.commands[cb.nofCommands].data.drawCommand.vao.vertexAttrib[0] = mesh.position;
+  cb.commands[cb.nofCommands].data.drawCommand.vao.vertexAttrib[1] = mesh.normal;
+  cb.commands[cb.nofCommands].data.drawCommand.vao.vertexAttrib[2] = mesh.texCoord;
+  cb.nofCommands++;
+}
+
+glm::mat4 combineMatrix(glm::mat4 const &m1, glm::mat4 const &m2)
+{
+  return m1 * m2;
+}
+
+uint32_t prepareNode(GPUMemory &mem, CommandBuffer &cb, Node const &node, Model const &model, glm::mat4 prubeznaMatice, uint32_t drawID)
+{
+  if (node.mesh >= 0)
+  {
+    Mesh const &mesh = model.meshes[node.mesh];
+    addDrawCommand(cb, mesh);
+    drawID++;
+
+    prubeznaMatice = combineMatrix(prubeznaMatice, node.modelMatrix);
+    glm::mat4 inverseModelMatrix = glm::transpose(glm::inverse(prubeznaMatice));
+
+    // test 36
+    // uniforms
+    mem.uniforms[10 + (drawID - 1) * 5].m4 = prubeznaMatice;
+    mem.uniforms[10 + (drawID - 1) * 5 + 1].m4 = inverseModelMatrix;
+    mem.uniforms[10 + (drawID - 1) * 5 + 2].v4 = mesh.diffuseColor;
+    mem.uniforms[10 + (drawID - 1) * 5 + 3].i1 = mesh.diffuseTexture;
+    mem.uniforms[10 + (drawID - 1) * 5 + 4].v1 = mesh.doubleSided;
+    
+  }
+
+  // rekurzivni zkoumani potomku
+  for(size_t i = 0; i < node.children.size(); i++)
+  {
+    drawID = prepareNode(mem, cb, node.children[i], model, prubeznaMatice, drawID);
+  }
+  return drawID;
+}
+
+/**
+ * @brief This function prepares model into memory and creates command buffer
+ *
+ * @param mem gpu memory
+ * @param commandBuffer command buffer
+ * @param model model structure
+ */
+//! [drawModel]
+void prepareModel(GPUMemory&mem,CommandBuffer&commandBuffer,Model const&model){
+  (void)mem;
+  (void)commandBuffer;
+  (void)model;
+  /// \todo Tato funkce připraví command buffer pro model a nastaví správně pamět grafické karty.<br>
+  /// Vaším úkolem je správně projít model a vložit vykreslovací příkazy do commandBufferu.
+  /// Zároveň musíte vložit do paměti textury, buffery a uniformní proměnné, které buffer command buffer využívat.
+  /// Bližší informace jsou uvedeny na hlavní stránce dokumentace a v testech.
+  
+
+  commandBuffer.commands[0].type = CommandType::CLEAR;
+  commandBuffer.commands[0].data.clearCommand.clearColor = true;
+  commandBuffer.commands[0].data.clearCommand.color = glm::vec4(0.1,0.15,0.1,1);
+  commandBuffer.commands[0].data.clearCommand.clearDepth = true;
+  commandBuffer.commands[0].data.clearCommand.depth = 10e10f;
+  commandBuffer.nofCommands = 1;
+  
+  glm::mat4 matrix = glm::mat4(1.f); // jednotkova matice
+  uint32_t drawID = 0;
+
+  size_t nofRoots = model.roots.size();
+  for (size_t i = 0; i < nofRoots; i++)
+  {
+    drawID = prepareNode(mem, commandBuffer, model.roots[i], model, matrix, drawID);
+  }
+
+  // test 32
+  mem.programs[0].vertexShader = drawModel_vertexShader;
+  mem.programs[0].fragmentShader = drawModel_fragmentShader;
+
+  // test 33
+  mem.programs[0].vs2fs[0] = AttributeType::VEC3; //position
+  mem.programs[0].vs2fs[1] = AttributeType::VEC3; //normal
+  mem.programs[0].vs2fs[2] = AttributeType::VEC2; //texCoord
+  mem.programs[0].vs2fs[3] = AttributeType::UINT; //drawID
+
+  // test 34
+  for (uint32_t i = 0; i < model.buffers.size(); i++)
+  {
+    mem.buffers[i].data = model.buffers[i].data;
+    mem.buffers[i].size = model.buffers[i].size;
+  }
+  
+  // test 35
+  for (uint32_t i = 0; i < model.textures.size(); i++)
+  {
+    mem.textures[i].channels = model.textures[i].channels;
+    mem.textures[i].data = model.textures[i].data;
+    mem.textures[i].height = model.textures[i].height;
+    mem.textures[i].width = model.textures[i].width;
+  }
+
+}
+//! [drawModel]
+
+/**
+ * @brief This function represents vertex shader of texture rendering method.
+ *
+ * @param outVertex output vertex
+ * @param inVertex input vertex
+ * @param si shader interface
+ */
+//! [drawModel_vs]
+void drawModel_vertexShader(OutVertex&outVertex,InVertex const&inVertex,ShaderInterface const&si){
+  (void)outVertex;
+  (void)inVertex;
+  (void)si;
+  /// \todo Tato funkce reprezentujte vertex shader.<br>
+  /// Vaším úkolem je správně trasnformovat vrcholy modelu.
+  /// Bližší informace jsou uvedeny na hlavní stránce dokumentace.
+  uint32_t drawID = inVertex.gl_DrawID;
+
+  glm::mat4 viewProjMat = si.uniforms[0].m4;
+  glm::mat4 modelMat = si.uniforms[10 + drawID * 5].m4;
+  glm::mat4 invModelMat = si.uniforms[10 + drawID * 5 + 1].m4;
+  glm::mat4 clipSpaceMat = combineMatrix(viewProjMat, modelMat);
+
+  outVertex.gl_Position = clipSpaceMat * (glm::vec4(inVertex.attributes[0].v3, 1.0f));
+  outVertex.attributes[0].v3 = glm::vec3(modelMat * (glm::vec4(inVertex.attributes[0].v3, 1.0f)));
+  outVertex.attributes[1].v3 = glm::vec3(invModelMat * (glm::vec4(inVertex.attributes[1].v3, 0.0f)));
+  outVertex.attributes[2].v2 = inVertex.attributes[2].v2;
+  outVertex.attributes[3].u1 = drawID;
+}
+//! [drawModel_vs]
+
+/**
+ * @brief This functionrepresents fragment shader of texture rendering method.
+ *
+ * @param outFragment output fragment
+ * @param inFragment input fragment
+ * @param si shader interface
+ */
+//! [drawModel_fs]
+void drawModel_fragmentShader(OutFragment&outFragment,InFragment const&inFragment,ShaderInterface const&si){
+  (void)outFragment;
+  (void)inFragment;
+  (void)si;
+  /// \todo Tato funkce reprezentujte fragment shader.<br>
+  /// Vaším úkolem je správně obarvit fragmenty a osvětlit je pomocí lambertova osvětlovacího modelu.
+  /// Bližší informace jsou uvedeny na hlavní stránce dokumentace.
+  uint32_t drawID = inFragment.attributes[3].u1;
+
+  glm::vec3 pos = inFragment.attributes[0].v3; 
+  glm::vec3 normal = inFragment.attributes[1].v3;
+  glm::vec3 light_pos = si.uniforms[1].v3;
+  glm::vec3 cam_pos = si.uniforms[2].v3;
+
+  int32_t texNum = si.uniforms[10 + drawID * 5 + 3].i1;
+
+  glm::vec2 texCoord = inFragment.attributes[2].v2;
+
+  float doubleSided = si.uniforms[10 + drawID * 5 + 4].v1;
+
+  if (doubleSided > 0.0f)
+  {
+    normal *= glm::sign(glm::dot(cam_pos - pos, normal));
+  }
+
+  glm::vec4 dColor;
+  if (texNum >= 0)
+  {
+    dColor = read_texture(si.textures[texNum], texCoord);
+  }
+  else
+  {
+    dColor = si.uniforms[10 + drawID * 5 + 2].v4;
+  }
+
+  glm::vec3 lightVec = light_pos - pos;
+
+  float const aF = 0.2f;
+  float dF = glm::clamp(glm::dot(glm::normalize(lightVec), glm::normalize(normal)), 0.f, 1.f);
+
+  glm::vec3 aL = glm::vec3(dColor) * aF;
+  glm::vec3 dL = glm::vec3(dColor) * dF;
+
+  outFragment.gl_FragColor = glm::vec4(aL + dL, dColor.a);
+}
+//! [drawModel_fs]
+
---
+++ IZG/izgProject/student/test/gpu.cpp
@@ -0,0 +1,461 @@
+/*!
+ * @file
+ * @brief This file contains implementation of gpu
+ *
+ * @author Tomáš Milet, imilet@fit.vutbr.cz
+ */
+
+#include <student/gpu.hpp>
+
+struct Triangle{
+  OutVertex points[3];
+};
+
+
+float max(float a, float b, float c){
+  float max = a;
+  if (b > max)
+  {
+    max = b;
+  }
+  if (c > max)
+  {
+    max = c;
+  }
+  return max;
+}
+
+
+float min(float a, float b, float c){
+  float min = a;
+  if (b < min)
+  {
+    min = b;
+  }
+  if (c < min)
+  {
+    min = c;
+  }
+  return min;
+}
+
+
+void rasterize(Frame &framebuffer, Triangle const &triangle, Program const &prg, DrawCommand cmd, ShaderInterface si){
+  glm::vec3 point[3];
+  glm::vec3 point2[3];
+  for (uint32_t i = 0; i < 3; i++)
+  {
+    /* Perspektivni deleni */
+    point[i].x = triangle.points[i].gl_Position.x / triangle.points[i].gl_Position.w;
+    point[i].y = triangle.points[i].gl_Position.y / triangle.points[i].gl_Position.w;
+    point[i].z = triangle.points[i].gl_Position.z / triangle.points[i].gl_Position.w;
+
+    point2[i].x = triangle.points[i].gl_Position.x / triangle.points[i].gl_Position.w;
+    point2[i].y = triangle.points[i].gl_Position.y / triangle.points[i].gl_Position.w;
+    point2[i].z = triangle.points[i].gl_Position.z / triangle.points[i].gl_Position.w;
+    
+    /* View-port transformace */
+    point[i].x = (point[i].x + 1) * (framebuffer.width / 2);
+    point[i].y = (point[i].y + 1) * (framebuffer.height / 2);
+  }
+
+
+  int32_t min_x = (int32_t) min(point[0].x, point[1].x, point[2].x);
+  int32_t max_x = (int32_t) max(point[0].x, point[1].x, point[2].x);
+  int32_t min_y = (int32_t) min(point[0].y, point[1].y, point[2].y);
+  int32_t max_y = (int32_t) max(point[0].y, point[1].y, point[2].y);
+  if (min_x < 0)
+  {
+    min_x = 0;
+  }
+  if (max_x >= framebuffer.width)
+  {
+    max_x = framebuffer.width - 1;
+  }
+  if (min_y < 0)
+  {
+    min_y = 0;
+  }
+  if (max_y >= framebuffer.height)
+  {
+    max_y = framebuffer.height - 1;
+  }
+
+  int flag = 0;
+  if (cmd.backfaceCulling)
+  {
+    //zahodit, kdyz je trojuhelnik clockwise
+    float area = (point[1].x - point[0].x) * (point[2].y - point[0].y) - (point[2].x - point[0].x) * (point[1].y - point[0].y);
+    if (area < 0.0f)
+    {
+      flag = 1;
+    }
+  }
+
+  for (uint32_t y = min_y; y <= max_y; y++)
+  {
+    for (uint32_t x = min_x; x <= max_x; x++)
+    {
+      float l0 = ((point[1].y - point[2].y) * ((float)x - point[2].x) + (point[2].x - point[1].x) * ((float)y - point[2].y)) / ((point[1].y - point[2].y) * (point[0].x - point[2].x) + (point[2].x - point[1].x) * (point[0].y - point[2].y));
+      float l1 = ((point[2].y - point[0].y) * ((float)x - point[2].x) + (point[0].x - point[2].x) * ((float)y - point[2].y)) / ((point[1].y - point[2].y) * (point[0].x - point[2].x) + (point[2].x - point[1].x) * (point[0].y - point[2].y));
+      float l2 = 1.0f - l0 - l1;
+      float l22 = l2;
+      if (l22 > -0.000001f) // obcas nevykreslil na 0. radku nektere pixely (kvuli -0,00...01 nesplnilo podminku)
+      {
+        l22 = abs(l22);
+      }
+      
+
+      if (l0 > 0.0f && l1 >= 0.0f && l22 >= 0.0f && !flag) //? >= >= >=
+      {
+        InFragment inFragment;
+        OutFragment outFragment;
+
+        inFragment.gl_FragCoord.x = (float)x + 0.5;
+        inFragment.gl_FragCoord.y = (float)y + 0.5;
+
+        inFragment.gl_FragCoord.z = (point2[0].z * l0 + point2[1].z * l1 + point2[2].z * l2);
+      
+
+        //for interpolate color of pixel we use center of it
+        l0 = ((point[1].y - point[2].y) * (inFragment.gl_FragCoord.x - point[2].x) + (point[2].x - point[1].x) * (inFragment.gl_FragCoord.y - point[2].y)) / ((point[1].y - point[2].y) * (point[0].x - point[2].x) + (point[2].x - point[1].x) * (point[0].y - point[2].y));
+        l1 = ((point[2].y - point[0].y) * (inFragment.gl_FragCoord.x - point[2].x) + (point[0].x - point[2].x) * (inFragment.gl_FragCoord.y - point[2].y)) / ((point[1].y - point[2].y) * (point[0].x - point[2].x) + (point[2].x - point[1].x) * (point[0].y - point[2].y));
+        l2 = 1.0f - l0 - l1;
+
+        float h0 = triangle.points[0].gl_Position.w;
+        float h1 = triangle.points[1].gl_Position.w;
+        float h2 = triangle.points[2].gl_Position.w;
+
+        float s = l0 / h0 + l1 / h1 + l2 / h2;
+
+        float new_l0 = l0 / (h0 * s);
+        float new_l1 = l1 / (h1 * s);
+        float new_l2 = l2 / (h2 * s);
+
+        for (uint32_t j = 0; j < maxAttributes; j++)
+        {
+          AttributeType frType = prg.vs2fs[j];
+          if (frType != AttributeType::EMPTY)
+          {
+            if (frType == AttributeType::FLOAT)
+            {
+              float vAttrib1 = triangle.points[0].attributes[j].v1;
+              float vAttrib2 = triangle.points[1].attributes[j].v1;
+              float vAttrib3 = triangle.points[2].attributes[j].v1;
+              inFragment.attributes[j].v1 = vAttrib1 * new_l0 + vAttrib2 * new_l1 + vAttrib3 * new_l2;
+            }
+            else if (frType == AttributeType::VEC2)
+            {
+              glm::vec2 vAttrib1 = triangle.points[0].attributes[j].v2;
+              glm::vec2 vAttrib2 = triangle.points[1].attributes[j].v2;
+              glm::vec2 vAttrib3 = triangle.points[2].attributes[j].v2;
+              inFragment.attributes[j].v2 = vAttrib1 * new_l0 + vAttrib2 * new_l1 + vAttrib3 * new_l2;
+            }
+            else if (frType == AttributeType::VEC3)
+            {
+              glm::vec3 vAttrib1 = triangle.points[0].attributes[j].v3;
+              glm::vec3 vAttrib2 = triangle.points[1].attributes[j].v3;
+              glm::vec3 vAttrib3 = triangle.points[2].attributes[j].v3;
+              inFragment.attributes[j].v3 = vAttrib1 * new_l0 + vAttrib2 * new_l1 + vAttrib3 * new_l2;
+            }
+            else if (frType == AttributeType::VEC4)
+            {
+              glm::vec4 vAttrib1 = triangle.points[0].attributes[j].v4;
+              glm::vec4 vAttrib2 = triangle.points[1].attributes[j].v4;
+              glm::vec4 vAttrib3 = triangle.points[2].attributes[j].v4;
+              inFragment.attributes[j].v4 = vAttrib1 * new_l0 + vAttrib2 * new_l1 + vAttrib3 * new_l2;
+            }
+            else // (frType == AttributeType::UINT/UVEC2/UVEC3/UVEC4)
+            {
+              inFragment.attributes[j] = triangle.points[0].attributes[j];
+            }
+          }
+        }
+
+        prg.fragmentShader(outFragment, inFragment, si);
+
+        
+        // PFO
+        // depth test
+        float depth = inFragment.gl_FragCoord.z;
+
+        //blending
+        float alpha = outFragment.gl_FragColor.a;
+        
+        uint32_t inc = y * framebuffer.width + x;
+
+        if (framebuffer.depth[inc] > depth)
+        {
+          if (alpha > 0.5f)
+          {
+            framebuffer.depth[inc] = depth;
+          }
+
+          framebuffer.color[inc * 4] = (((framebuffer.color[inc * 4] + 0.0001f) / 255.f) * (1 - alpha) + outFragment.gl_FragColor.r * alpha) * 255.f; // + 0.0001f je tam kvuli zaokrouhlovani
+          framebuffer.color[inc * 4 + 1] = (((framebuffer.color[inc * 4 + 1] + 0.0001f) / 255.f) * (1 - alpha) + outFragment.gl_FragColor.g * alpha) * 255.f;
+          framebuffer.color[inc * 4 + 2] = (((framebuffer.color[inc * 4 + 2] + 0.0001f) / 255.f) * (1 - alpha) + outFragment.gl_FragColor.b * alpha) * 255.f;
+        }
+      }
+    }
+  }
+}
+
+
+void draw(GPUMemory &mem, DrawCommand cmd, uint32_t nofDraws){
+  Program prg = mem.programs[cmd.programID];
+  Triangle triangle;
+
+  for (uint32_t i = 0; i < cmd.nofVertices; i++)
+  {
+    InVertex inVertex;
+    OutVertex outVertex;
+    inVertex.gl_DrawID = nofDraws;
+
+    /* Computing vertexID */
+    if (cmd.vao.indexBufferID < 0)
+    {
+      // neni zapnuty indexovani
+      inVertex.gl_VertexID = i;
+    }
+    else
+    {
+      // je zapnuty indexovani
+      int32_t bufferID = cmd.vao.indexBufferID;
+      void const *indexBuffer = mem.buffers[bufferID].data;
+      
+      if (cmd.vao.indexType == IndexType::UINT8)
+      {
+        uint8_t *ind = (uint8_t *) ((uint8_t *)indexBuffer + cmd.vao.indexOffset);
+        inVertex.gl_VertexID = ind[i];
+      }
+      else if (cmd.vao.indexType == IndexType::UINT16)
+      {
+        uint16_t *ind = (uint16_t *) ((uint8_t *)indexBuffer + cmd.vao.indexOffset);
+        inVertex.gl_VertexID = ind[i];
+      }
+      else // (cmd.vao.indexType == IndexType::UINT32)
+      {
+        uint32_t *ind = (uint32_t *) ((uint8_t *)indexBuffer + cmd.vao.indexOffset);
+        inVertex.gl_VertexID = ind[i];
+      }
+    }
+
+    ShaderInterface si;
+    si.uniforms = mem.uniforms;
+    si.textures = mem.textures;
+
+
+    /* Reading vertex attributes */
+    for (uint8_t j = 0; j < maxAttributes; j++)
+    {
+      AttributeType vaType = cmd.vao.vertexAttrib[j].type;
+      if (vaType != AttributeType::EMPTY)
+      {
+        int32_t vaBuffer = cmd.vao.vertexAttrib[j].bufferID;
+        uint64_t vaOffset = cmd.vao.vertexAttrib[j].offset;
+        uint64_t vaStride = cmd.vao.vertexAttrib[j].stride;
+        void const *vertexBuffer = mem.buffers[vaBuffer].data;
+
+        if (vaType == AttributeType::FLOAT)
+        {
+          float *vAttrib = (float *) ((uint8_t *)vertexBuffer + vaOffset + vaStride * inVertex.gl_VertexID);
+          inVertex.attributes[j].v1 = *vAttrib;
+        }
+        else if (vaType == AttributeType::VEC2)
+        {
+          glm::vec2 *vAttrib = (glm::vec2 *) ((uint8_t *)vertexBuffer + vaOffset + vaStride * inVertex.gl_VertexID);
+          inVertex.attributes[j].v2 = *vAttrib;
+        }
+        else if (vaType == AttributeType::VEC3)
+        {
+          glm::vec3 *vAttrib = (glm::vec3 *) ((uint8_t *)vertexBuffer + vaOffset + vaStride * inVertex.gl_VertexID);
+          inVertex.attributes[j].v3 = *vAttrib;
+        }
+        else if (vaType == AttributeType::VEC4)
+        {
+          glm::vec4 *vAttrib = (glm::vec4 *) ((uint8_t *)vertexBuffer + vaOffset + vaStride * inVertex.gl_VertexID);
+          inVertex.attributes[j].v4 = *vAttrib;
+        }
+        else if (vaType == AttributeType::UINT)
+        {
+          uint32_t *vAttrib = (uint32_t *) ((uint8_t *)vertexBuffer + vaOffset + vaStride * inVertex.gl_VertexID);
+          inVertex.attributes[j].u1 = *vAttrib;
+        }
+        else if (vaType == AttributeType::UVEC2)
+        {
+          glm::uvec2 *vAttrib = (glm::uvec2 *) ((uint8_t *)vertexBuffer + vaOffset + vaStride * inVertex.gl_VertexID);
+          inVertex.attributes[j].u2 = *vAttrib;
+        }
+        else if (vaType == AttributeType::UVEC3)
+        {
+          glm::uvec3 *vAttrib = (glm::uvec3 *) ((uint8_t *)vertexBuffer + vaOffset + vaStride * inVertex.gl_VertexID);
+          inVertex.attributes[j].u3 = *vAttrib;
+        }
+        else // (vaType == AttributeType::UVEC4)
+        {
+          glm::uvec4 *vAttrib = (glm::uvec4 *) ((uint8_t *)vertexBuffer + vaOffset + vaStride * inVertex.gl_VertexID);
+          inVertex.attributes[j].u4 = *vAttrib;
+        }
+      }
+    }
+    prg.vertexShader(outVertex, inVertex, si);
+    
+    triangle.points[i % 3] = outVertex;
+    if (i % 3 == 2)
+    {
+      /* CLIPPING */
+      bool isPointBehind[3] = {false, false, false};
+      uint32_t pointsBehind = 0;
+      for (uint32_t vec = 0; vec < 3; vec++)
+      {
+        if (-triangle.points[vec].gl_Position.w > triangle.points[vec].gl_Position.z)
+        {
+          isPointBehind[vec] = true;
+          pointsBehind++;
+        }
+      }
+
+      switch (pointsBehind)
+      {
+        case 0:
+          // rasterize everything
+          rasterize(mem.framebuffer, triangle, prg, cmd, si);
+          break;
+        
+        case 1:
+          // rasterize two new triangles (2 points are on near plane)
+          {
+            uint32_t vecOut;
+            for (uint32_t vec = 0; vec < 3; vec++)
+            {
+              if (isPointBehind[vec])
+              {
+                vecOut = vec; // only 1 point is outside
+              }
+            }
+
+            glm::vec4 newPos2[2];
+            uint32_t vecsIn[2];
+            uint32_t nextV = 0;
+            for (uint32_t vec = 0; vec < 3; vec++) // computing new position of points (now I have 4 points)
+            {
+              if (!isPointBehind[vec])
+              {
+                float t = (-triangle.points[vec].gl_Position.w - triangle.points[vec].gl_Position.z) / (triangle.points[vecOut].gl_Position.w - triangle.points[vec].gl_Position.w + triangle.points[vecOut].gl_Position.z - triangle.points[vec].gl_Position.z);
+                newPos2[nextV] = triangle.points[vec].gl_Position + t * (triangle.points[vecOut].gl_Position - triangle.points[vec].gl_Position);
+                vecsIn[nextV] = vec;
+                nextV++;
+              }
+            }
+            triangle.points[vecOut].gl_Position = newPos2[0];
+            rasterize(mem.framebuffer, triangle, prg, cmd, si);
+
+            triangle.points[vecsIn[0]].gl_Position = newPos2[1];
+            rasterize(mem.framebuffer, triangle, prg, cmd, si);
+          }
+          break;
+        
+        case 2:
+          // rasterize new triangle (1 point is on near plane)
+          uint32_t vecIn;
+          for (uint32_t vec = 0; vec < 3; vec++)
+          {
+            if (!isPointBehind[vec])
+            {
+              vecIn = vec; // only 1 point is inside
+            }
+          }
+
+          for (uint32_t vec = 0; vec < 3; vec++)
+          {
+            if (isPointBehind[vec])
+            {
+              float t = (-triangle.points[vecIn].gl_Position.w - triangle.points[vecIn].gl_Position.z) / (triangle.points[vec].gl_Position.w - triangle.points[vecIn].gl_Position.w + triangle.points[vec].gl_Position.z - triangle.points[vecIn].gl_Position.z);
+              glm::vec4 newPos = triangle.points[vecIn].gl_Position + t * (triangle.points[vec].gl_Position - triangle.points[vecIn].gl_Position);
+              triangle.points[vec].gl_Position = newPos;
+            }
+          }
+          rasterize(mem.framebuffer, triangle, prg, cmd, si);
+          break;
+        
+        case 3:
+          // dont rasterize
+          break;
+      }
+    }
+  }
+}
+
+
+void clear(GPUMemory &mem, ClearCommand cmd){
+  if(cmd.clearColor)
+  {
+    float red = cmd.color.r;
+    float green = cmd.color.g;
+    float blue = cmd.color.b;
+    float alpha = cmd.color.a;
+    
+    for (uint32_t x = 0; x < mem.framebuffer.width * mem.framebuffer.height * 4; x += 4)
+    {
+      mem.framebuffer.color[x] = (uint8_t) (red * 255.f);
+      mem.framebuffer.color[x + 1] = (uint8_t) (green * 255.f);
+      mem.framebuffer.color[x + 2] = (uint8_t) (blue * 255.f);
+      mem.framebuffer.color[x + 3] = (uint8_t) (alpha * 255.f);
+    }
+  }
+  if(cmd.clearDepth)
+  {
+    float depth = cmd.depth;
+
+    for (uint32_t x = 0; x < mem.framebuffer.width * mem.framebuffer.height; x++)
+    {
+      mem.framebuffer.depth[x] = depth;
+    }
+  }
+}
+
+
+//! [gpu_execute]
+void gpu_execute(GPUMemory&mem,CommandBuffer &cb){
+  (void)mem;
+  (void)cb;
+  /// \todo Tato funkce reprezentuje funkcionalitu grafické karty.<br>
+  /// Měla by umět zpracovat command buffer, čistit framebuffer a kresli.<br>
+  /// mem obsahuje paměť grafické karty.
+  /// cb obsahuje command buffer pro zpracování.
+  /// Bližší informace jsou uvedeny na hlavní stránce dokumentace.
+  uint32_t nofDraws = 0;
+  for (uint32_t i = 0; i < cb.nofCommands; i++)
+  {
+    if (cb.commands[i].type == CommandType::CLEAR)
+    {
+      clear(mem, cb.commands[i].data.clearCommand);
+    }
+    else if (cb.commands[i].type == CommandType::DRAW)
+    {
+      draw(mem, cb.commands[i].data.drawCommand, nofDraws++);
+    }
+  }
+}
+//! [gpu_execute]
+
+/**
+ * @brief This function reads color from texture.
+ *
+ * @param texture texture
+ * @param uv uv coordinates
+ *
+ * @return color 4 floats
+ */
+glm::vec4 read_texture(Texture const&texture,glm::vec2 uv){
+  if(!texture.data)return glm::vec4(0.f);
+  auto uv1 = glm::fract(uv);
+  auto uv2 = uv1*glm::vec2(texture.width-1,texture.height-1)+0.5f;
+  auto pix = glm::uvec2(uv2);
+  //auto t   = glm::fract(uv2);
+  glm::vec4 color = glm::vec4(0.f,0.f,0.f,1.f);
+  for(uint32_t c=0;c<texture.channels;++c)
+    color[c] = texture.data[(pix.y*texture.width+pix.x)*texture.channels+c]/255.f;
+  return color;
+}
+
