c8eb69b409b91cb8847953628888498b6c4a262e
MivojBlazek <MivojBlazek@seznam.cz>
2024-10-08 20:51:33 +0200

    IZV updated name in project part 1

--- IZV/project_part1/part01.py
+++ IZV/project_part1/part01.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
 IZV cast1 projektu
-Autor: Michal
+Autor: Michal Blažek <xblaze38>
 
 Detailni zadani projektu je v samostatnem projektu e-learningu.
 Nezapomente na to, ze python soubory maji dane formatovani.
